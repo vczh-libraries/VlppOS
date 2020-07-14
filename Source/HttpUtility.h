db4f8a3b6cf4b4d028cb52a647889777d658451d
Zihan Chen <vczh@163.com>
2020-07-14 02:33:47 -0700

    ...

--- Source/HttpUtility.h
+++ Source/HttpUtility.h
@@ -17,7 +17,7 @@ namespace vl
 HTTP Utility
 ***********************************************************************/
 
-	/// <summary>A type representing an http requiest.</summary>
+	/// <summary>An http requiest.</summary>
 	class HttpRequest
 	{
 		typedef collections::Array<char>					BodyBuffer;
@@ -27,37 +27,37 @@ HTTP Utility
 		/// <summary>Name of the server, like "gaclib.net".</summary>
 		WString				server;
 		/// <summary>Port of the server, like 80.</summary>
-		vint				port;
-		/// <summary>Query of the request, like "/GettingStart.html".</summary>
+		vint				port = 0;
+		/// <summary>Query of the request, like "/index.html".</summary>
 		WString				query;
-		/// <summary>Set to true if the request uses SSL.</summary>
-		bool				secure;
-		/// <summary>User name to authorize. Set to empty if you don't want to provide it.</summary>
+		/// <summary>Set to true if the request uses SSL, or https.</summary>
+		bool				secure = false;
+		/// <summary>User name to authorize. Set to empty if authorization is not needed.</summary>
 		WString				username;
-		/// <summary>Password to authorize. Set to empty if you don't want to provide it.</summary>
+		/// <summary>Password to authorize. Set to empty if authorization is not needed.</summary>
 		WString				password;
 		/// <summary>HTTP method, like "GET", "POST", "PUT", "DELETE", etc.</summary>
 		WString				method;
-		/// <summary>Cookie. Set to empty if you don't want to provide it.</summary>
+		/// <summary>Cookie. Set to empty if cookie is not needed.</summary>
 		WString				cookie;
-		/// <summary>Request body. This is a binary array using an array container to char.</summary>
+		/// <summary>Request body. This is a byte array.</summary>
 		BodyBuffer			body;
 		/// <summary>Content type, like "text/xml".</summary>
 		WString				contentType;
-		/// <summary>Accept type list, elements of it like "text/xml".</summary>
+		/// <summary>Accept type list, elements like "text/xml".</summary>
 		StringList			acceptTypes;
 		/// <summary>A dictionary to contain extra headers.</summary>
 		HeaderMap			extraHeaders;
 
 		/// <summary>Create an empty request.</summary>
-		HttpRequest();
+		HttpRequest() = default;
 
 		/// <summary>Set <see cref="server"/>, <see cref="port"/>, <see cref="query"/> and <see cref="secure"/> fields for you using an URL.</summary>
 		/// <returns>Returns true if this operation succeeded.</returns>
 		/// <param name="inputQuery">The URL.</param>
 		bool				SetHost(const WString& inputQuery);
 
-		/// <summary>Fill the body with a text using UTF-8 encoding.</summary>
+		/// <summary>Fill the text body in UTF-8.</summary>
 		/// <param name="bodyString">The text to fill.</param>
 		void				SetBodyUtf8(const WString& bodyString);
 	};
@@ -68,28 +68,34 @@ HTTP Utility
 		typedef collections::Array<char>		BodyBuffer;
 	public:
 		/// <summary>Status code, like 200.</summary>
-		vint				statusCode;
-		/// <summary>Response body. This is a binary array using an array container to char.</summary>
+		vint				statusCode = 0;
+		/// <summary>Response body. This is a byte array.</summary>
 		BodyBuffer			body;
 		/// <summary>Returned cookie from the server.</summary>
 		WString				cookie;
 
-		HttpResponse();
+		HttpResponse() = default;
 
-		/// <summary>If you believe the server returns a text in UTF-8, use it to decode the body.</summary>
+		/// <summary>Get the text body, encoding is assumed to be UTF-8.</summary>
 		/// <returns>The response body as text.</returns>
 		WString				GetBodyUtf8();
 	};
 
 	/// <summary>Send an http request and receive a response.</summary>
-	/// <returns>Returns true if this operation succeeded. Even the server returns 404 will be treated as success, because you get the response.</returns>
-	/// <param name="request">The request.</param>
-	/// <param name="response">The response.</param>
+	/// <returns>Returns true if this operation succeeded, even when the server returns 404.</returns>
+	/// <param name="request">The request to send.</param>
+	/// <param name="response">Returns the response.</param>
+	/// <remarks>This function will block the calling thread until the respons is returned.</remarks>
 	extern bool				HttpQuery(const HttpRequest& request, HttpResponse& response);
 
 	/// <summary>Encode a text as part of the url. This function can be used to create arguments in an URL.</summary>
 	/// <returns>The encoded text.</returns>
 	/// <param name="query">The text to encode.</param>
+	/// <remarks>
+	/// When a character is not a digit or a letter,
+	/// it is first encoded to UTF-8,
+	/// then each byte is written as "%" with two hex digits.
+	///</remarks>
 	extern WString			UrlEncodeQuery(const WString& query);
 }
 
