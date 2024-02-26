eaee9005d2b520155c69b8f450e140d45573b9e0
vczh <vczh@outlook.com>
2024-02-25 23:08:23 -0800

    ...

--- Source/Encoding/CharFormat/CharFormat.Linux.cpp
+++ Source/Encoding/CharFormat/CharFormat.Linux.cpp
@@ -46,5 +46,48 @@ MbcsEncoder
 			}
 			return chars;
 		}
+
+/***********************************************************************
+Helper Functions
+***********************************************************************/
+
+		extern bool CanBeMbcs(unsigned char* buffer, vint size);
+		extern bool CanBeUtf8(unsigned char* buffer, vint size);
+		extern bool CanBeUtf16(unsigned char* buffer, vint size, bool& hitSurrogatePairs);
+		extern bool CanBeUtf16BE(unsigned char* buffer, vint size, bool& hitSurrogatePairs);
+		
+/***********************************************************************
+TestEncoding
+***********************************************************************/
+
+		extern void TestEncodingInternal(
+			unsigned char* buffer,
+			vint size,
+			BomEncoder::Encoding& encoding,
+			bool containsBom,
+			bool utf16HitSurrogatePairs,
+			bool utf16BEHitSurrogatePairs,
+			bool roughMbcs,
+			bool roughUtf8,
+			bool roughUtf16,
+			bool roughUtf16BE
+			)
+		{
+			if (roughUtf16 && roughUtf16BE && !roughUtf8)
+			{
+				if (utf16BEHitSurrogatePairs && !utf16HitSurrogatePairs)
+				{
+					encoding = BomEncoder::Utf16BE;
+				}
+				else
+				{
+					encoding = BomEncoder::Utf16;
+				}
+			}
+			else
+			{
+				encoding = BomEncoder::Utf8;
+			}
+		}
 	}
 }
