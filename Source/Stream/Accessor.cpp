de015aad4761dc7ec6b0ee988923cd5bc06db219
vczh <vczh@outlook.com>
2024-02-26 01:06:46 -0800

    ...

--- Source/Stream/Accessor.cpp
+++ Source/Stream/Accessor.cpp
@@ -19,16 +19,16 @@ TextReader_<T>
 		template<typename T>
 		ObjectString<T> TextReader_<T>::ReadString(vint length)
 		{
-			T* buffer=new T[length+1];
-			vint i=0;
-			for(;i<length;i++)
+			T* buffer = new T[length + 1];
+			vint i = 0;
+			for (; i < length; i++)
 			{
-				if((buffer[i]=ReadChar())==0)
+				if ((buffer[i] = ReadChar()) == 0)
 				{
 					break;
 				}
 			}
-			buffer[i]=0;
+			buffer[i] = 0;
 			ObjectString<T> result(buffer);
 			delete[] buffer;
 			return result;
@@ -39,36 +39,36 @@ TextReader_<T>
 		{
 			ObjectString<T> result;
 			auto buffer = new T[65537];
-			buffer[0]=0;
-			vint i=0;
-			while(true)
+			buffer[0] = 0;
+			vint i = 0;
+			while (true)
 			{
-				T c=ReadChar();
-				if(c==L'\n' || c==0)
+				T c = ReadChar();
+				if (c == L'\n' || c == 0)
 				{
-					buffer[i]=0;
-					result+=buffer;
-					buffer[0]=0;
-					i=0;
+					buffer[i] = 0;
+					result += buffer;
+					buffer[0] = 0;
+					i = 0;
 					break;
 				}
 				else
 				{
-					if(i==65536)
+					if (i == 65536)
 					{
-						buffer[i]=0;
-						result+=buffer;
-						buffer[0]=0;
-						i=0;
+						buffer[i] = 0;
+						result += buffer;
+						buffer[0] = 0;
+						i = 0;
 					}
-					buffer[i++]=c;
+					buffer[i++] = c;
 				}
 			}
-			result+=buffer;
+			result += buffer;
 			delete[] buffer;
-			if(result.Length()>0 && result[result.Length()-1]==L'\r')
+			if (result.Length() > 0 && result[result.Length() - 1] == L'\r')
 			{
-				return result.Left(result.Length()-1);
+				return result.Left(result.Length() - 1);
 			}
 			else
 			{
@@ -81,32 +81,32 @@ TextReader_<T>
 		{
 			ObjectString<T> result;
 			auto buffer = new T[65537];
-			buffer[0]=0;
-			vint i=0;
-			while(true)
+			buffer[0] = 0;
+			vint i = 0;
+			while (true)
 			{
-				T c=ReadChar();
-				if(c==0)
+				T c = ReadChar();
+				if (c == 0)
 				{
-					buffer[i]=0;
-					result+=buffer;
-					buffer[0]=0;
-					i=0;
+					buffer[i] = 0;
+					result += buffer;
+					buffer[0] = 0;
+					i = 0;
 					break;
 				}
 				else
 				{
-					if(i==65536)
+					if (i == 65536)
 					{
-						buffer[i]=0;
-						result+=buffer;
-						buffer[0]=0;
-						i=0;
+						buffer[i] = 0;
+						result += buffer;
+						buffer[0] = 0;
+						i = 0;
 					}
-					buffer[i++]=c;
+					buffer[i++] = c;
 				}
 			}
-			result+=buffer;
+			result += buffer;
 			delete[] buffer;
 			return result;
 		}
@@ -118,7 +118,7 @@ TextWriter_<T>
 		template<typename T>
 		void TextWriter_<T>::WriteString(const T* string, vint charCount)
 		{
-			while(*string)
+			while (*string)
 			{
 				WriteChar(*string++);
 			}
@@ -161,7 +161,7 @@ TextWriter_<T>
 		template<typename T>
 		void TextWriter_<T>::WriteString(const ObjectString<T>& string)
 		{
-			if(string.Length())
+			if (string.Length())
 			{
 				WriteString(string.Buffer(), string.Length());
 			}
@@ -194,10 +194,10 @@ TextWriter_<T>
 			void WriteBorderLine(TextWriter_<T>& writer, Array<vint>& columnWidths, vint columns)
 			{
 				writer.WriteChar(L'+');
-				for(vint i=0;i<columns;i++)
+				for (vint i = 0; i < columns; i++)
 				{
-					vint c=columnWidths[i];
-					for(vint j=0;j<c;j++)
+					vint c = columnWidths[i];
+					for (vint j = 0; j < c; j++)
 					{
 						writer.WriteChar(L'-');
 					}
@@ -209,29 +209,29 @@ TextWriter_<T>
 			template<typename T>
 			void WriteContentLine(TextWriter_<T>& writer, Array<vint>& columnWidths, vint rowHeight, vint columns, Array<ObjectString<T>>& tableByRow, vint startRow)
 			{
-				vint cellStart=startRow*columns;
-				for(vint r=0;r<rowHeight;r++)
+				vint cellStart = startRow * columns;
+				for (vint r = 0; r < rowHeight; r++)
 				{
 					writer.WriteChar(L'|');
-					for(vint c=0;c<columns;c++)
+					for (vint c = 0; c < columns; c++)
 					{
-						const T* cell=tableByRow[cellStart+c].Buffer();
-						for(vint i=0;i<r;i++)
+						const T* cell = tableByRow[cellStart + c].Buffer();
+						for (vint i = 0; i < r; i++)
 						{
-							if(cell) cell=::wcsstr(cell, L"\r\n");
-							if(cell) cell+=2;
+							if (cell) cell = ::wcsstr(cell, L"\r\n");
+							if (cell) cell += 2;
 						}
 
 						writer.WriteChar(L' ');
-						vint length=0;
-						if(cell)
+						vint length = 0;
+						if (cell)
 						{
-							const T* end=::wcsstr(cell, L"\r\n");
-							length=end?end-cell:(vint)wcslen(cell);
+							const T* end = ::wcsstr(cell, L"\r\n");
+							length = end ? end - cell : (vint)wcslen(cell);
 							writer.WriteString(cell, length);
 						}
 
-						for(vint i=columnWidths[c]-2;i>=length;i--)
+						for (vint i = columnWidths[c] - 2; i >= length; i--)
 						{
 							writer.WriteChar(L' ');
 						}
@@ -248,43 +248,43 @@ TextWriter_<T>
 		{
 			Array<vint> rowHeights(rows);
 			Array<vint> columnWidths(columns);
-			for(vint i=0;i<rows;i++) rowHeights[i]=0;
-			for(vint j=0;j<columns;j++) columnWidths[j]=0;
+			for (vint i = 0; i < rows; i++) rowHeights[i] = 0;
+			for (vint j = 0; j < columns; j++) columnWidths[j] = 0;
 
-			for(vint i=0;i<rows;i++)
+			for (vint i = 0; i < rows; i++)
 			{
-				for(vint j=0;j<columns;j++)
+				for (vint j = 0; j < columns; j++)
 				{
-					ObjectString<T> text=tableByRow[i*columns+j];
-					const T* reading=text.Buffer();
-					vint width=0;
-					vint height=0;
+					ObjectString<T> text = tableByRow[i * columns + j];
+					const T* reading = text.Buffer();
+					vint width = 0;
+					vint height = 0;
 
-					while(reading)
+					while (reading)
 					{
 						height++;
-						const T* crlf=::wcsstr(reading, L"\r\n");
-						if(crlf)
+						const T* crlf = ::wcsstr(reading, L"\r\n");
+						if (crlf)
 						{
-							vint length=crlf-reading+2;
-							if(width<length) width=length;
-							reading=crlf+2;
+							vint length = crlf - reading + 2;
+							if (width < length) width = length;
+							reading = crlf + 2;
 						}
 						else
 						{
-							vint length=(vint)wcslen(reading)+2;
-							if(width<length) width=length;
-							reading=0;
+							vint length = (vint)wcslen(reading) + 2;
+							if (width < length) width = length;
+							reading = 0;
 						}
 					}
 
-					if(rowHeights[i]<height) rowHeights[i]=height;
-					if(columnWidths[j]<width) columnWidths[j]=width;
+					if (rowHeights[i] < height) rowHeights[i] = height;
+					if (columnWidths[j] < width) columnWidths[j] = width;
 				}
 			}
 
 			WriteBorderLine(*this, columnWidths, columns);
-			for(vint i=0;i<rows;i++)
+			for (vint i = 0; i < rows; i++)
 			{
 				WriteContentLine(*this, columnWidths, rowHeights[i], columns, tableByRow, i);
 				WriteBorderLine(*this, columnWidths, columns);
@@ -298,33 +298,33 @@ StringReader_<T>
 		template<typename T>
 		void StringReader_<T>::PrepareIfLastCallIsReadLine()
 		{
-			if(lastCallIsReadLine)
+			if (lastCallIsReadLine)
 			{
-				lastCallIsReadLine=false;
-				if(current<string.Length() && string[current]==L'\r') current++;
-				if(current<string.Length() && string[current]==L'\n') current++;
+				lastCallIsReadLine = false;
+				if (current < string.Length() && string[current] == L'\r') current++;
+				if (current < string.Length() && string[current] == L'\n') current++;
 			}
 		}
 
 		template<typename T>
 		StringReader_<T>::StringReader_(const ObjectString<T>& _string)
-			:string(_string)
-			,current(0)
-			,lastCallIsReadLine(false)
+			: string(_string)
+			, current(0)
+			, lastCallIsReadLine(false)
 		{
 		}
 
 		template<typename T>
 		bool StringReader_<T>::IsEnd()
 		{
-			return current==string.Length();
+			return current == string.Length();
 		}
 
 		template<typename T>
 		T StringReader_<T>::ReadChar()
 		{
 			PrepareIfLastCallIsReadLine();
-			if(IsEnd())
+			if (IsEnd())
 			{
 				return 0;
 			}
@@ -338,16 +338,16 @@ StringReader_<T>
 		ObjectString<T> StringReader_<T>::ReadString(vint length)
 		{
 			PrepareIfLastCallIsReadLine();
-			if(IsEnd())
+			if (IsEnd())
 			{
 				return L"";
 			}
 			else
 			{
-				vint remain=string.Length()-current;
-				if(length>remain) length=remain;
-				ObjectString<T> result=string.Sub(current, length);
-				current+=length;
+				vint remain = string.Length() - current;
+				if (length > remain) length = remain;
+				ObjectString<T> result = string.Sub(current, length);
+				current += length;
 				return result;
 			}
 		}
@@ -356,22 +356,22 @@ StringReader_<T>
 		ObjectString<T> StringReader_<T>::ReadLine()
 		{
 			PrepareIfLastCallIsReadLine();
-			if(IsEnd())
+			if (IsEnd())
 			{
 				return L"";
 			}
 			else
 			{
-				vint lineEnd=current;
-				while(lineEnd<string.Length())
+				vint lineEnd = current;
+				while (lineEnd < string.Length())
 				{
-					T c=string[lineEnd];
-					if(c==L'\r' || c==L'\n') break;
+					T c = string[lineEnd];
+					if (c == L'\r' || c == L'\n') break;
 					lineEnd++;
 				}
-				ObjectString<T> result=string.Sub(current, lineEnd-current);
-				current=lineEnd;
-				lastCallIsReadLine=true;
+				ObjectString<T> result = string.Sub(current, lineEnd - current);
+				current = lineEnd;
+				lastCallIsReadLine = true;
 				return result;
 			}
 		}
@@ -379,7 +379,7 @@ StringReader_<T>
 		template<typename T>
 		ObjectString<T> StringReader_<T>::ReadToEnd()
 		{
-			return ReadString(string.Length()-current);
+			return ReadString(string.Length() - current);
 		}
 
 /***********************************************************************
@@ -388,25 +388,25 @@ StreamReader_<T>
 
 		template<typename T>
 		StreamReader_<T>::StreamReader_(IStream& _stream)
-			:stream(&_stream)
+			: stream(&_stream)
 		{
 		}
 
 		template<typename T>
 		bool StreamReader_<T>::IsEnd()
 		{
-			return stream==0;
+			return stream == nullptr;
 		}
 
 		template<typename T>
 		T StreamReader_<T>::ReadChar()
 		{
-			if(stream)
+			if (stream)
 			{
-				T buffer=0;
-				if(stream->Read(&buffer, sizeof(buffer))==0)
+				T buffer = 0;
+				if (stream->Read(&buffer, sizeof(buffer)) == 0)
 				{
-					stream=0;
+					stream = nullptr;
 					return 0;
 				}
 				else
@@ -439,7 +439,7 @@ StreamWriter_<T>
 		template<typename T>
 		void StreamWriter_<T>::WriteString(const T* string, vint charCount)
 		{
-			stream->Write((void*)string, charCount*sizeof(*string));
+			stream->Write((void*)string, charCount * sizeof(*string));
 		}
 
 /***********************************************************************
