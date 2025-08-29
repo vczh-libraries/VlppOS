86dd0e779cebe8bb2fe3c81ee6dba16251a1de8f
vczh <vczh@outlook.com>
2025-08-29 15:47:16 -0700

    [NEW SCRUM]
    Created the initial FileSystem.Injectable.cpp file with license and include. Updated Copilot_Scrum.md with a detailed design and task breakdown for implementing an injectable file system infrastructure, including interface design, Linux and Windows platform implementations, and integration steps.

---
+++ Source/FileSystem.Injectable.cpp
@@ -0,0 +1,6 @@
+/***********************************************************************
+Author: Zihan Chen (vczh)
+Licensed under https://github.com/vczh-libraries/License
+***********************************************************************/
+
+#include "FileSystem.h"
