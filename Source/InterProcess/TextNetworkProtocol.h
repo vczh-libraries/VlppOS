cf80fb7da6e7e4986577fa94fbeaaaaa3ae2cee2
vczh <vczh@outlook.com>
2026-05-12 02:30:48 -0700

    ...

--- Source/InterProcess/TextNetworkProtocol.h
+++ Source/InterProcess/TextNetworkProtocol.h
@@ -27,12 +27,51 @@ namespace vl::inter_process
 		virtual void				OnReconnectedUnsafe() = 0;
 	};
 
+	struct NetworkPackage
+	{
+		Nullable<vint>				clientId;
+		WString						channelName;
+		WString						messageBody;
+
+		static inline NetworkPackage Create(Nullable<vint> _clientId, const WString& _channelName, const WString& _messageBody)
+		{
+			NetworkPackage package;
+			package.clientId = std::move(_clientId);
+			package.channelName = _channelName;
+			package.messageBody = _messageBody;
+			return package;
+		}
+
+		static inline void Parse(const WString& str, NetworkPackage& package)
+		{
+#define ERROR_MESSAGE_PREFIX L"vl::inter_process::NetworkPackage::Parse(const WString&, NetworkPackage&)#"
+			const wchar_t* reading = str.Buffer();
+
+			const wchar_t* afterClientId = wcschr(reading, L';');
+			CHECK_ERROR(afterClientId != nullptr, ERROR_MESSAGE_PREFIX L"Invalid package format.");
+			if (afterClientId == reading)
+			{
+				package.clientId.Reset();
+			}
+			else
+			{
+				package.clientId = wtoi(str.Left((vint)(afterClientId - reading)));
+			}
+
+			const wchar_t* afterChannelName = wcschr(afterClientId + 1, L';');
+			CHECK_ERROR(afterChannelName != nullptr, ERROR_MESSAGE_PREFIX L"Invalid package format.");
+			package.channelName = str.Sub((vint)(afterClientId - reading + 1), (vint)(afterChannelName - afterClientId - 1));
+			package.messageBody = str.Right(str.Length() - (vint)(afterChannelName - reading + 1));
+#undef ERROR_MESSAGE_PREFIX
+		}
+	};
+
 	class INetworkProtocol : public virtual Interface
 	{
 	public:
 		virtual void				InstallCallback(INetworkProtocolCallback* callback) = 0;
 		virtual void				BeginReadingLoopUnsafe() = 0;
-		virtual void				SendString(Nullable<vint> clientId, const WString& channelName, const WString& messageBody) = 0;
+		virtual void				SendString(const NetworkPackage& networkPackage) = 0;
 	};
 }
 
--- TODO.md
+++ TODO.md
@@ -6,6 +6,7 @@
 - `InterProcessorClientTest` project.
 - `INetworkProtocol` implementations currently accept one client, need improvement.
   - Need to send `clientId` (empty to broadcast), `channelName`, `str`.
+  - Only `*Client` inherits from `INetworkProtocol`.
   - `NamedPipeServer`.
   - `HttpServer`.
 
