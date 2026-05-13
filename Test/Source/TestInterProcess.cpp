0608138fe6db0f4d3577591ae651a88c37b3a230
vczh <vczh@outlook.com>
2026-05-13 07:10:07 -0700

    Update TestInterProcess.cpp

--- Test/Source/TestInterProcess.cpp
+++ Test/Source/TestInterProcess.cpp
@@ -10,12 +10,50 @@ using namespace vl::inter_process;
 
 namespace mynamespace
 {
+	class TimeoutThread : public Thread
+	{
+	public:
+		atomic_vint			threadCounter = 0;
+		bool				timeout = false;
+
+	protected:
+
+		void Run() override
+		{
+			auto startTime = DateTime::LocalTime();
+			while (DateTime::LocalTime().osMilliseconds - startTime.osMilliseconds < 5000)
+			{
+				if (threadCounter == 3) return;
+			}
+			timeout = true;
+		}
+	};
+
 	void RunTextNetworkProtocol(
 		Func<Ptr<INetworkProtocolServer>()> createServer,
 		Func<Ptr<INetworkProtocolClient>()> createClient
 		)
 	{
+		auto timeoutThread = Ptr(new TimeoutThread);
 
+		ThreadPoolLite::QueueLambda([&]()
+		{
+			timeoutThread->threadCounter++;
+		});
+
+		ThreadPoolLite::QueueLambda([&]()
+		{
+			timeoutThread->threadCounter++;
+		});
+
+		ThreadPoolLite::QueueLambda([&]()
+		{
+			timeoutThread->threadCounter++;
+		});
+
+		timeoutThread->Start();
+		timeoutThread->Wait();
+		TEST_ASSERT(!timeoutThread->timeout);
 	}
 }
 using namespace mynamespace;
